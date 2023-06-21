143259874b6987a1baa70f034196130c195a689f
Valongo <viniciusvalongo1@hotmail.com>
2023-06-21 14:29:54 -0300

    Delecao de usuario com DELETE

--- src/main/java/com/viniciusvalongo/workshopmongo/resources/UserResource.java
+++ src/main/java/com/viniciusvalongo/workshopmongo/resources/UserResource.java
@@ -4,10 +4,9 @@ import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import javax.annotation.PostConstruct;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -49,5 +48,11 @@ public class UserResource {
 		return ResponseEntity.created(uri).build();
 	}
 	
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity <Void> delete(@PathVariable String id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
 
 }
