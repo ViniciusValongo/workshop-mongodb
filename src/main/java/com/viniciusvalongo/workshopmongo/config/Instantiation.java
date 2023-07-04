3622434b13fdbedbd929ded2f21fdb414f031bdb
Valongo <viniciusvalongo1@hotmail.com>
2023-07-03 21:56:48 -0300

    Criando entity Post com User aninhado

--- src/main/java/com/viniciusvalongo/workshopmongo/config/Instantiation.java
+++ src/main/java/com/viniciusvalongo/workshopmongo/config/Instantiation.java
@@ -1,16 +1,24 @@
 package com.viniciusvalongo.workshopmongo.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.viniciusvalongo.workshopmongo.domain.Post;
 import com.viniciusvalongo.workshopmongo.domain.User;
+import com.viniciusvalongo.workshopmongo.repository.PostRepository;
 import com.viniciusvalongo.workshopmongo.repository.UserRepository;
 
 @Configuration
 public class Instantiation implements CommandLineRunner{
+	
+	@Autowired
+	private PostRepository postRepository;
+	
 	
 	@Autowired
 	private UserRepository userRepository;
@@ -18,14 +26,23 @@ public class Instantiation implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		
+		
 		userRepository.deleteAll();
+		postRepository.deleteAll();
 		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");	
 		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "partiu viagem" , "Vou viajar para Sao Paulo, Abracos!", maria);
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia" , "Acordei feliz hoje!", maria);
+		
 		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		postRepository.saveAll(Arrays.asList(post1,post2));
 	}
 	
 
--- src/main/java/com/viniciusvalongo/workshopmongo/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/viniciusvalongo/workshopmongo/resources/exception/ResourceExceptionHandler.java
@@ -1,3 +1,4 @@
+
 package com.viniciusvalongo.workshopmongo.resources.exception;
 
 import javax.servlet.http.HttpServletRequest;
